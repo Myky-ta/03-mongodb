388f9416b80e53cc8f511702cae9f54633f79865
Bolotnuk <nikita24tenith@gmail.com>
2026-03-04 21:45:26 +0200

    start

--- db/db.js
+++ db/db.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI);
     console.log("Database connection successful");
-  } catch (error) {
-    console.error("Database connection error:", error.message);
-    process.exit(1);
+  } catch (err) {
+    console.error("Database connection error:", err);
+    process.exit(1);  
   }
 };
 
