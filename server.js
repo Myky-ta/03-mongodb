d03896203f6a772540050a095e417b6f2bf478a6
Myky-ta <nikita24tenith@gmail.com>
2026-03-10 09:16:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-// Підключення бібліотек через require
 const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
+const connectDB = require("./db/db");
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 
-// Підключення до MongoDB
-mongoose.connect(process.env.DB_URI)
-  .then(() => console.log("Database connection successful"))
-  .catch(err => console.error("Database connection error:", err));
+// Підключення до бази
+connectDB();
 
-// Простий маршрут
-app.get("/", (req, res) => {
-  res.send("Hello from CommonJS server!");
+// Імпорт роутера
+const contactsRouter = require("./routes/contacts");
+
+// Підключення роутера до додатку
+app.use("/api/contacts", contactsRouter);
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Запуск сервера
